19b91991a7eca39d4c1ac3b5007cd52c67434dcf
danjuan-77 <youngertutu@gmail.com>
2025-08-13 11:41:48 +0800

    update

--- infer_gpu2.sh
+++ infer_gpu2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=3
-python batch_inference.py --save-dir infer_results/opens2s_sft_bs1_acc4_lr2e-5_ep1_5000/wav --model-path /mnt/buffer/tuwenming/checkpoints/OpenS2S/OpenS2S_SFT_20250811_213511_bs1_acc4_lr2e-5_ep1/checkpoint-5000
+python batch_inference.py --save-dir infer_results/opens2s_sft_bs1_acc4_lr2e-5_ep1_5000/wav --model-path /mnt/buffer/tuwenming/checkpoints/OpenS2S/OpenS2S_SFT_20250811_213511_bs1_acc4_lr2e-5_ep1/checkpoint-5000/
 
 # nohup bash infer_gpu2.sh > ../UltraVoice_dev/logs/opens2s_base_infer_gpu2_$(date +%Y%m%d%H%M%S).log 2>&1 &
